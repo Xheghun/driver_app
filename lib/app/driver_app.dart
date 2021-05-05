ab582e2639980b5159cf1287978778c9a375f8ff
Xheghun <ysenruf@gmail.com>
2021-05-05 16:43:46 +0400

    project setup

---
+++ lib/app/driver_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DriverApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Driver App',
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:driver_app/app/driver_app.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(DriverApp());
+}

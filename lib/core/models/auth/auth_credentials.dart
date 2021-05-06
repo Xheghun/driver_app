88910905d95c68bd7cf7baf4f30e412d3cf2c3b7
Xheghun <ysenruf@gmail.com>
2021-05-06 12:56:31 +0400

    firebase init

---
+++ lib/core/models/auth/auth_credentials.dart
@@ -0,0 +1,6 @@
+class AuthCredentials {
+  final String email;
+  final String password;
+
+  AuthCredentials(this.email, this.password);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:driver_app/app/driver_app.dart';
 import 'package:flutter/material.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(DriverApp());
 }

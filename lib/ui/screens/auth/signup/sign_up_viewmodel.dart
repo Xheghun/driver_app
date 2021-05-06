a607f4bfe7ed3251a341ecd928d2eacd035225b3
Xheghun <ysenruf@gmail.com>
2021-05-06 23:21:58 +0400

    sign up viewmodel

---
+++ lib/ui/screens/auth/signup/sign_up_viewmodel.dart
@@ -0,0 +1,31 @@
+import 'package:driver_app/app/base_viewmodel/base_viewmodel.dart';
+import 'package:driver_app/core/entities/auth/auth_credentials.dart';
+import 'package:driver_app/core/use_case/auth/auth_usecase.dart';
+import 'package:driver_app/ui/helpers/notifier.dart';
+import 'package:driver_app/ui/screens/main/main_screeen.dart';
+import 'package:flutter/material.dart';
+
+class SignUpViewModel extends BaseViewModel {
+  AuthUseCase authUseCase;
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _fullnameController = TextEditingController();
+
+  void login(BuildContext context) async {
+    var credentials = AuthCredentials(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+        fullname: _fullnameController.text.trim());
+
+    var result = await authUseCase.createUser(credentials);
+
+    result.fold((failure) {
+      //error
+      showFlushBar(context, title: "SignUp Error", message: failure.message);
+    }, (r) {
+      //success
+      Navigator.pushNamed(context, MainScreen.routeName);
+    });
+  }
+}
